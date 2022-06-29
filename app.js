078d4e50dff34fe6010912e5a9a145f3348b3bcb
Lucas <bithome1@outlook.com>
2022-06-29 17:46:28 -0300

    adicionado express.json()

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const productController = require('./controllers/ProductController');
 
 const app = express();
 
+app.use(express.json());
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
