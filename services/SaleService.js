2061ca25c7789d0ff4681c3811e6c6d69f091fd6
Lucas <bithome1@outlook.com>
2022-06-30 18:09:15 -0300

    adicionado findAllSales

--- services/SaleService.js
+++ services/SaleService.js
@@ -1,4 +1,8 @@
+const saleModel = require('../models/SaleModel');
+
 const createSales = async (body) => {
+  // Conta todos os id's das vendas
+  const listAllSales = saleModel.findAllSales();
   await Promise.all(body.map((sale) => saleModel.addSales(sale)));
 };
 
