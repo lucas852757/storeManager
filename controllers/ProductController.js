cbae23942f4538bfd1899e92860ce286b533f939
Lucas <bithome1@outlook.com>
2022-06-30 17:02:09 -0300

    removido postSales

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -25,19 +25,8 @@ const postProducts = async (req, res, next) => {
   }
 };
 
-const postSales = async (req, res, next) => {
-  try {
-    const { body } = req;
-    const sales = productService.addtSales(body);
-    return res.status(201).json(sales);
-  } catch (error) {
-    next(error);
-  }
-};
-
 module.exports = {
   getOneProduct,
   getAllProducts,
   postProducts,
-  postSales,
 };
