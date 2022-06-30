98535e29f7203e69769d70c09418fa6bb561dc27
Lucas <bithome1@outlook.com>
2022-06-30 11:49:07 -0300

    adicionado postProducts no module.exports

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -15,7 +15,16 @@ const getAllProducts = async (req, res) => {
     return res.status(200).json(products);
 };
 
+const postProducts = async (req, res, next) => {
+  try {
+    const products = await productService.createProduct();
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getOneProduct,
   getAllProducts,
+  postProducts,
 };
