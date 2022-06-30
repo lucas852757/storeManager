1c2f2d9665e3ab7ff45db96141b5d6046dbca27e
Lucas <bithome1@outlook.com>
2022-06-30 18:32:32 -0300

    adicionado query

--- models/SaleModel.js
+++ models/SaleModel.js
@@ -5,6 +5,12 @@ const findAllSales = async () => {
   return Object.values(sales[0])[0]; // retorna valor
 };
 
+const addSales = async (id, { productId, quantity }) => {
+  const query = 'insert into sales_products (sale_id, product_Id, quantity) values (?,?,?)';
+  await connection.execute(query, [id, productId, quantity]);
+};
+
 module.exports = {
   findAllSales,
+  addSales,
 };
