1402bda559ca9847b6c446dab27a56aa7091f01e
Lucas <bithome1@outlook.com>
2022-06-30 18:23:00 -0300

    adicionado um contador

--- services/SaleService.js
+++ services/SaleService.js
@@ -4,6 +4,8 @@ const createSales = async (body) => {
   // Conta todos os id's das vendas
   let listAllSales = saleModel.findAllSales();
 
+  // Soma mais 1
+  listAllSales += 1;
   await Promise.all(body.map((sale) => saleModel.addSales(sale)));
 };
 
