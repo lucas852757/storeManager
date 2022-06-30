24993bd11d7393092ef69a108fc968fe4b188ff4
Lucas <bithome1@outlook.com>
2022-06-30 17:23:57 -0300

    renomeado addtSales para createSales

---
+++ controllers/SaleController.js
@@ -0,0 +1,13 @@
+const postSales = async (req, res, next) => {
+  try {
+    const { body } = req;
+    const sales = saleService.createSales(body);
+    return res.status(201).json(sales);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  postSales,
+};
