1c35f33e123f4d6d77cc62003bf1e5e636e527d3
Lucas <bithome1@outlook.com>
2022-06-30 18:11:41 -0300

    const listAllSales mudado para let listAllSales

--- services/SaleService.js
+++ services/SaleService.js
@@ -2,7 +2,8 @@ const saleModel = require('../models/SaleModel');
 
 const createSales = async (body) => {
   // Conta todos os id's das vendas
-  const listAllSales = saleModel.findAllSales();
+  let listAllSales = saleModel.findAllSales();
+
   await Promise.all(body.map((sale) => saleModel.addSales(sale)));
 };
 
