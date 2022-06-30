811e3beefd4662a0cb1f87544cd8a32cadc354a2
Lucas <bithome1@outlook.com>
2022-06-30 15:16:57 -0300

    adicionado lógica para validar erros do Joi

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -3,6 +3,16 @@ const error = (err, req, res, _next) => {
   if (err.status) {
     return res.status(err.status).json({ message: err.message });
   }
+
+  if (
+    err.isJoi
+    && (err.details[0].type.includes('.min'))
+  ) {
+    // console.log('-->',err.details[0]);
+    res.status(422).json({ message: err.message });
+  } else {
+    res.status(400).json({ message: err.message });
+  }
 };
 
 module.exports = error;
