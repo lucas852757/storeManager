e2aa63d03b9d6a4e4e54932c5641c8f12fe3a250
Lucas <bithome1@outlook.com>
2022-06-30 18:00:02 -0300

    corrigido module.exports

--- models/SaleModel.js
+++ models/SaleModel.js
@@ -1,1 +1,10 @@
 const connection = require('./connection');
+
+const findAllSales = async () => {
+  const [sales] = await connection.query('select count(id) from sales');
+  return Object.values(sales[0])[0]; // retorna valor
+};
+
+module.exports = {
+  findAllSales,
+};
