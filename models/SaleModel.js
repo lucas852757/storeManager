f51db2d74d4159105bb9eb4b22117acf0a4be2c3
Lucas <bithome1@outlook.com>
2022-06-30 21:11:09 -0300

    verificado função findAllSales

--- models/SaleModel.js
+++ models/SaleModel.js
@@ -2,6 +2,7 @@ const connection = require('./connection');
 
 const findAllSales = async () => {
   const [sales] = await connection.query('select count(id) from sales');
+
   return Object.values(sales[0])[0]; // retorna valor
 };
 
