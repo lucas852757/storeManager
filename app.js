be7807b520ca9909b2e687987787b61ea33fe563
Lucas <bithome1@outlook.com>
2022-07-02 15:11:19 -0300

    criado caminho /sales/:id

--- app.js
+++ app.js
@@ -23,5 +23,7 @@ app.get('/products', productController.getAllProducts);
 
 app.post('/products', productController.postProducts);
 app.post('/sales', saleController.postSales);
+
+app.get('/sales/:id');
 app.use(error);
 module.exports = app;
