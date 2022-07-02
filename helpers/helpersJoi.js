25e7c18a69b600a41afbf1509475cdaeffd6b53b
Lucas <bithome1@outlook.com>
2022-07-01 21:42:19 -0300

    só foi colocado remo vido o .only no __tests__/req06-createSale.test.js

--- helpers/helpersJoi.js
+++ helpers/helpersJoi.js
@@ -3,17 +3,16 @@ const Joi = require('joi');
 // Identifica e lança o erro
 const errorsOfJoi = (body) => {
  body.forEach(({ productId, quantity }) => {
-  const schema = Joi.object({
-    productId: Joi.number().not().empty().required(),
-    quantity: Joi.number().min(1).greater(1).not()
-.empty()
+   const schema = Joi.object({
+     productId: Joi.number().not().empty().required(),
+     quantity: Joi.number().min(1).not().empty()
 .required(),
-  });
-  const { error } = schema.validate({ productId, quantity });
-  if (error) {
-    throw error;
-  }
-}); 
+   }); // https://github.com/sideway/joi/issues/2371
+   const { error } = schema.validate({ productId, quantity });
+   if (error) {
+     throw error;
+   }
+ }); 
 };
 
 module.exports = { errorsOfJoi };
