4330f787d9616fec47ebce0d4b546f018adf9d67
Lucas <bithome1@outlook.com>
2022-07-11 10:32:57 -0300

    model/SaleMode -> getSale

---
+++ tests/unit/models/salesModels.test.js
@@ -0,0 +1,24 @@
+const sinon = require("sinon");
+const { expect } = require("chai");
+const connection = require("../../../models/connection");
+
+const SaleModel = require('../../../models/SaleModel');
+
+describe("model/SaleModel", () => {
+  beforeEach(sinon.restore);
+
+  describe("getSale", () => {
+       it("retorna uma lista vazia,quando não encontra avenda", async () => {
+      sinon.stub(connection, "query").resolves([[]]);
+      const response = await SaleModel.getSale();
+
+      expect(response).to.be.empty;
+       });
+    
+    it("quando encontra a venda, retorna uma lista de objetos com as vendas", async () => {
+      sinon.stub(connection, "query").resolves([[{}]]);
+      const response = await SaleModel.getSale(0);
+      expect(response[0]).to.deep.keys('productId', 'quantity');
+    });
+  });
+});
